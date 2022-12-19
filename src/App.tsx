741a7f5baa58e44d3c103ba533253dc5b2f56cbd
VladimirErohin <vovaerohin1@gmail.com>
2022-12-20 02:02:33 +0300

    improve app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import {Counter} from "./components/Counter";
-import {Home} from "./components/home-page/Home";
+import {Home} from "./pages/homePage/Home";
 
 function App() {
 
